8b7558f5752deefd7049d9a6c138d4349f1d360a
Mariyam <88830386+mariyam627@users.noreply.github.com>
2024-02-19 21:19:52 +0500

    Update q5.dart

--- q5.dart
+++ q5.dart
@@ -1,10 +1,9 @@
-// Write a program that calculates the sum of all the digits in a given
-// number using a while loop.
+// Write a program that calculates the sum of all the digits in a given number using a while loop.
 // Example:
 // Input: 12345
 // Output: Sum of digits: 15
 void main(){
-
+int number = 12345;
 
   
 }
