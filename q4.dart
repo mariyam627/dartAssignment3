99b211629f4572e267e9a3a3c1e624755b9c1909
Mariyam <88830386+mariyam627@users.noreply.github.com>
2024-02-19 21:17:00 +0500

    Update q4.dart

--- q4.dart
+++ q4.dart
@@ -4,7 +4,13 @@
 // Input: 5
 // Output: Factorial of 5 is 120
 void main(){
+//The factorial of n is denoted by n! and calculated by multiplying the 
+//integer numbers from 1 to n. The formula for n factorial is n! = n × (n - 1)!.
 
-
-  
+  int number = 5;
+  int factorial = 1;
+  for(int i=0; i<-number; i++){
+    factorial*=i
+    }
+  print("Factorial of $number is $factorial");
 }
